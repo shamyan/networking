3e335d1b1b8bfb866ef61185166c0d7fcfdaf70e
Harutyun Shamyan <shamyan.harut@gmail.com>
2023-02-14 17:34:44 +0400

    initial commit

---
+++ networking/networkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkingApp.swift
+//  networking
+//
+//  Created by Harutyun Shamyan on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct networkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
